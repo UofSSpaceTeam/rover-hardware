fb3a35d452a79d09a844e391e8042057e35180c6
ausshir <ausshir@gmail.com>
2017-05-14 14:05:09 -0600

    Small fixes to commands for pushing all HW repos together

--- Tools/pushHwRepos.sh
+++ Tools/pushHwRepos.sh
@@ -3,9 +3,9 @@ response=${response,,}
 
 if [[ "$response" =~ ^(yes|y|'')$ ]]; then
 	echo 'Pushing Symbols...';
-	(cd ../../Kicad-symbols/; git diff-index --quiet HEAD -- && echo 'Nothing to do' || (read -r -p "Commit message for Symbols: " symbols_msg; git commit -m "$symbols_msg"; git push));
+	(cd ../../Kicad-symbols/; git diff-index --quiet HEAD -- && echo 'Nothing to do' || (git add -A; read -r -p "Commit message for Symbols: " symbols_msg; git commit -m "$symbols_msg"; git push));
 	echo 'Pushing Footprints...'
-	(cd ../../USST-footprints.pretty; git diff-index --quiet HEAD -- && echo 'Nothing to do' || (read -r -p "Commit message for Symbols: " symbols_msg; git commit -m "$symbols_msg"; git push));
+	(cd ../../USST-footprints.pretty; git diff-index --quiet HEAD -- && echo 'Nothing to do' || (git add -A; read -r -p "Commit message for Footprints: " symbols_msg; git commit -m "$symbols_msg"; git push));
 	echo 'Pushing Designs...'
-	(cd ..; git diff-index --quiet HEAD -- && echo 'Nothing to do' || (read -r -p "Commit message for Symbols: " symbols_msg; git commit -m "$symbols_msg"; git push));
+	(cd ..; git diff-index --quiet HEAD -- && echo 'Nothing to do' || (git add -A; read -r -p "Commit message for HW Designs: " symbols_msg; git commit -m "$symbols_msg"; git push));
 fi
